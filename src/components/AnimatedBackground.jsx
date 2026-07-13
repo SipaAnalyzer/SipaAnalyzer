c6ece6db869de8438554f473ba093624bec87656
Saclagofast1 <saclagofast1@gmail.com>
2026-07-13 09:04:58 +0200

    Desynchronise background brackets without increasing density

--- src/components/AnimatedBackground.jsx
+++ src/components/AnimatedBackground.jsx
@@ -1,10 +1,3 @@
 export default function AnimatedBackground() {
-  return (
-    <div className="animated-background" aria-hidden="true">
-      <span className="bracket-layer bracket-layer-a" />
-      <span className="bracket-layer bracket-layer-b" />
-      <span className="bracket-layer bracket-layer-c" />
-      <span className="bracket-layer bracket-layer-d" />
-    </div>
-  );
+  return <div className="animated-background" aria-hidden="true" />;
 }
