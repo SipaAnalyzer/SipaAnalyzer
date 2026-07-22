76a63c769cd21ad7028d36e5f4bbb26b557e0c3a
Saclagofast1 <saclagofast1@gmail.com>
2026-07-22 11:02:08 +0200

    Corriger le cadrage top de la vague dashboard

--- src/components/KPICards.jsx
+++ src/components/KPICards.jsx
@@ -61,7 +61,7 @@ export default function KPICards({
             <div className="sipa-card-waves" aria-hidden="true">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 24 150 28"
+                viewBox="0 26 150 26"
                 preserveAspectRatio="none"
                 shapeRendering="auto"
               >
